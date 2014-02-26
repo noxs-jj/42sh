82c0aea07cdee8da90b5ee15b348566c870429f4
Nakiboudine MOHAMED-ABAINE <nmohamed@e2r7p9.42.fr>
2014-02-26 15:00:45 +0100

    fixed norme in ft_cd

--- ft_cd.h
+++ ft_cd.h
@@ -6,14 +6,14 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/25 18:17:41 by nmohamed          #+#    #+#             */
-/*   Updated: 2014/02/26 14:03:03 by nmohamed         ###   ########.fr       */
+/*   Updated: 2014/02/26 14:59:49 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_CD_H
 # define FT_CD_H
-#include <unistd.h>
-#include <sys/stat.h>
+# include <unistd.h>
+# include <sys/stat.h>
 
 # define FT_CD_SUCCESS 1
 # define FT_CD_NOEXIST 2
