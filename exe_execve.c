1435ec24f118572f75bdd81125af443c51870962
nmohamed <nmohamed@student.42.fr>
2014-03-04 15:22:18 +0100

    fixed stupid errors lol

--- exe_execve.c
+++ exe_execve.c
@@ -6,7 +6,7 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 18:15:27 by nmohamed          #+#    #+#             */
-/*   Updated: 2014/03/04 15:16:54 by nmohamed         ###   ########.fr       */
+/*   Updated: 2014/03/04 15:22:06 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ static char	*exe_search_exe(t_data *d)
 	ft_strdel(&tmp);
 	return (NULL);
 }
-*/
+
 int			exe_execve(t_data *d)
 {
 	int			error;
@@ -72,12 +72,12 @@ int			exe_execve(t_data *d)
 		if (error == ERR_DENIED)
 			WR(2, "access denied\n");
 	}
-	if (d->argv != NULL && d->argv[0] != NULL)
+	if (d->argv != NULL && d->argv[0] != NULL && error == SUCCESS)
 	{
 		execve(d->argv[0], d->argv, d->env);
 		ft_exit(d, "could not execute");
 	}
 	else if (d->argv[0] == NULL)
-		WR(2, "command not tmp\n");
+		WR(2, "command not found\n");
 	return (-1);
 }
